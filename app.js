d831017caa0a0426e2e9519350494245f48f16d2
Marcos Cachis <marcoscachis@gmail.com>
2024-07-01 21:49:20 -0600

    setup deploy vercel

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 import * as dotenv from "dotenv";
 import { connectDB } from "./db/index.js";
 import { router as teamsRouter } from "./routes/teams.js";
+import cors from "cors";
 
 dotenv.config();
 const port = process.env.PORT || 3000;
@@ -9,9 +10,15 @@ const port = process.env.PORT || 3000;
 connectDB();
 
 const app = express();
+app.use(cors(
+    {
+        origin: "*",
+        methods: "GET,POST,PATCH,DELETE",
+    }
+));
 app.use(express.json());
-app.use("/api/teams", teamsRouter);
+app.use(express.urlencoded({ extended: true }));
+app.get("/", (req, res) => res.send("Hello World!"));
+app.use("/api/teams/", teamsRouter);
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+export default app;
